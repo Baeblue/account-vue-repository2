95b553a0e015f5236181d2efb00903c80759c488
Baeblue <seungmin941215@gmail.com>
2020-05-25 16:02:38 +0900

    Example Line Chart

--- src/front/src/components/MonthlyGraph.vue
+++ src/front/src/components/MonthlyGraph.vue
@@ -2,21 +2,21 @@
   <div class="graph">
     <div class="header">
       <div class="subject">
-        <h3>월별 지출액 그래프</h3>
+        <h3>월별 지출액 그래프</h3> <!-- total을 월별로 따로 가져와야하나 -->
       </div>
     </div>
 
     <div class="line-chart">
       <graph-line
-              :width="1000"
-              :height="800"
-              :shape="'normal'"
-              :axis-min="0"
-              :axis-max="500000"
-              :axis-full-mode="true"
-              :labels="labels"
-              :names="names"
-              :values="values">
+        :width="1050"
+        :height="700"
+        :shape="'normal'"
+        :axis-min="0"
+        :axis-max="500000"
+        :axis-full-mode="true"
+        :labels="labels"
+        :names="names"
+        :values="values">
         <note :text="'월별 지출액 추이'"></note>
         <tooltip :names="names" :position="'right'"></tooltip>
         <legends :names="names"></legends>
@@ -31,18 +31,31 @@
     name: "MonthlyGraph",
     data() {
       return {
-        accounts: [],
-        total: 0,
-        selectedDate: {
-          year: "",
-          month: "",
-        },
-        labels: [ this.year + '/' + this.month ],
-        names: ['총액'],
-        values: [ this.total ]
+        labels: [ '2020. 02.', '2020. 03.', '2020. 04.', '2020. 05.' ],    // -> 월
+        names: [ "누적 지출액" ],    // -> 누적액
+        values: [
+          [ 200000, 300000, 250000, 300000 ],                     // -> 월별 누적액이 하나씩.
+        ]
       }
-    },
+    }
   };
+
+  // export default {
+  //   name: "MonthlyGraph",
+  //   data() {
+  //     return {
+  //       accounts: [],
+  //       total: 0,
+  //       selectedDate: {
+  //         year: "",
+  //         month: "",
+  //       },
+  //       labels: [ this.year + '/' + this.month ],
+  //       names: ['총액'],
+  //       values: [ this.total ]
+  //     }
+  //   },
+  // };
 </script>
 
 <style scoped>
